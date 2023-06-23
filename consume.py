28b0a9b26fa82878d8425dc97d87ef0d00e6bc4d
Lren1300 <reneaul@mail.gvsu.edu>
2023-06-23 16:45:52 -0400

    Polished code and attempted to implement proper logging

--- consume.py
+++ consume.py
@@ -1,36 +1,77 @@
-# *********** Imports *************#
+"""
+ REST API Consumption Problem: With the given API Url,
+ write a python script to call the api with a set
+ of inputs, and consume the response given.
+"""
+
+
+#***********Imports*************#
 import requests
 import json
+import entryexiterrorlogging
+import google.cloud.logging
+import time
 
-# **********Local Variables************#
-url =  "https://esldp-sit2-west.corp.cvscaremark.com:5889/DBPLService/subscription/fetchFlagPropertyValue/V1%27"
 
-# Function to consume a POST response from a REST api call
+#********Initialize Clients***********#
+logging_client = google.cloud.logging.Client()
+logging_client.get_default_handler()
+logging_client.setup_logging()
+
+
+#**********Local Variables************#
+url = "https://esldp-sit2-west.corp.cvscaremark.com:5889/DBPLService/subscription/fetchFlagPropertyValue/V1%27"
+cloud_function_name = "cf-consume-response"
+
 
 def consume_response():
-    # store the input of the name and type
-    flag_name = input("Enter a name: ")
-    type_name = input("Enter a type: ")
+    """
+     consume_response: Function to consume a
+     POST response from a REST api POST call
+    """
+    start_time = time.time()
+    loggingdict = {"entryType" : "Subscription_Platform", "opName": cloud_function_name,
+                   "src": "CVS_Cloud_Functions", "action": "CF_Consume_Respsonse", "programName": "CF_Consume_Respsonse"}
 
-    # create a json with these two variables as data
-    input_json = {"name": flag_name, "type": type_name}
-    json_request = json.dumps(input_json)
+    # Invoke entry logging
+    entryexiterrorlogging.entryEventLog(loggingdict, "CVSEVENT")
 
-    # Initialize the headers
-    headers = {
-        "Accept": "application/json",
-        "Cache-Control": "no-cache",
-        "Content-Type": "application/json",
-        "env": "UAT1"
+    try:
+        # store the input of the name and type
+        flag_name = input("Enter a name: ")
+        type_name = input("Enter a type: ")
 
-    }
+        # create a dictionary with these two variables as data
+        input_json = {"name": flag_name, "type": type_name}
 
-    # send the POST call to the url with the given data
-    response = requests.post(url, data=json_request, headers=headers)
+        # use dumps to convert dict into a json object
+        json_request = json.dumps(input_json)
 
-    # print the response of the POST call
-    print(response.json())
+        # Initialize the headers to specified values
+        headers = {
+            "Accept": "application/json",
+            "Cache-Control": "no-cache",
+            "Content-Type": "application/json",
+            "env": "UAT1"
 
-#function to call our functionality
-def start_process():
-    consume_response()
+        }
+
+        # send the POST call to the url, json, and headers
+        response = requests.post(url, data=json_request, headers=headers)
+
+        # print the response of the POST call for testing
+        print(response.json())
+
+    except Exception as exe:
+        # invoke error logging
+        loggingdict.update({"Exception": format(exe)})
+        entryexiterrorlogging.errorEventLog(loggingdict, "CVSEVENT")
+
+    finally:
+        # invoke exit logging
+        resp_time = time.time() - start_time
+        loggingdict.update({"respTime": str(round(resp_time, 3)) + "secs"})
+
+
+# Driver Code to test above function
+consume_response()
